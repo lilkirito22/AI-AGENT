0ddbcf5f7e65e7f53680bbd4eea04a8f3332d887
Daniel Chaves <dccastro.7@gmail.com>
2025-05-28 17:34:39 -0300

    Implement research assistant functionality with agent execution and response handling

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
@@ -15,4 +16,35 @@ class ResearchResponse(BaseModel):
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
+parser = PydanticOutputParser(pydantic_object= ResearchResponse)
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+  llm=llm,
+  prompt=prompt,
+  tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent,
+                               tools=[],
+                               verbose=True)
+raw_response = agent_executor.invoke({
+    "query": "What is the impact of climate change on polar bear populations?"
+})
+
+print(raw_response)
